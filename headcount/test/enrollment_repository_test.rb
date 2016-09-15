4a11a83d4cbd77f8ca0a0591c44179e5fd243d05
jdconrad89 <jdconrad@liberty.edu>
2016-09-14 22:45:06 -0600

    Add files via upload

--- headcount/test/enrollment_repository_test.rb
+++ headcount/test/enrollment_repository_test.rb
@@ -1,15 +1,34 @@
 require 'simplecov'
 SimpleCov.start
-require 'minitest/autorun'
-require 'minitest/pride'
+
 require_relative '../lib/enrollment_repository.rb'
 
 class EnrollmentRepositoryTest < Minitest::Test
 
   def test_enrollment_repository_exists
-    enroll = EnrollmentRepository.new
+    er = EnrollmentRepository.new
 
 
-    assert_equal EnrollmentRepository, enroll.class
+    assert_equal EnrollmentRepository, er.class
   end
-end
+
+  def test_dr_can_load_data
+  er = EnrollmentRepository.new
+      er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"
+      }
+    })
+    assert_equal 181, er.enrollment.values.length
+  end
+
+  def test_it_can_find_a_district_by_name
+    er = EnrollmentRepository.new
+      er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+    assert_equal "ACADEMY 20", enrollment.name
+  end
