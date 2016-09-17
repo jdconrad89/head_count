793858e5cdeff0b7013f6d5580cf7c75c8aec652
Brett Rivers <riverswb@gmail.com>
2016-09-17 11:04:12 -0600

    enrollment.kindergarten_participation_by_year(year) is still not correct

--- headcount/lib/enrollment.rb
+++ headcount/lib/enrollment.rb
@@ -8,19 +8,32 @@ attr_reader :name, :kindergarten_participation
   end
 
   def kindergarten_participation_by_year
-    part_values = @kindergarten_participation.values
-    cleaned = []
-    part_values.each do |year|
-      cleaned << (year * 1000).floor / 1000.0
+    if @kindergarten_participation.class == String
+      participation = @kindergarten_participation.to_f
+    else
+      participation = @kindergarten_participation.values
     end
-    @kindergarten_participation.keys.zip(cleaned).to_h
+    part_values = participation
+    cleaned = []
     binding.pry
+    if part_values.class != Float
+      part_values.each do |year|
+        cleaned << (year * 1000).floor / 1000.0
+      end
+    end
+    if @kindergarten_participation.class != String
+      @kindergarten_participation.keys.zip(cleaned).to_h
+    end
+    # binding.pry
   end
 
   def kindergarten_participation_in_year(year)
-    binding.pry
-    if @kindergarten_participation.key?(year)
-      binding.pry
+    # binding.pry
+    if @kindergarten_participation.class == String
+      @kindergarten_participation.to_f
+      kindergarten_participation_by_year[year]
+    elsif @kindergarten_participation.key?(year)
+      # binding.pry
       kindergarten_participation_by_year[year]
     else
       nil
--- headcount/test/enrollment_test.rb
+++ headcount/test/enrollment_test.rb
@@ -1,26 +1,42 @@
-# require_relative 'test_helper'
-# require_relative '../lib/enrollment.rb'
-#
-# class EnrollmentTest < Minitest::Test
-#
-#   def test_enrollment_holds_enrollment_data_for_single_district
-#     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
-#
-#     assert_equal "ACADEMY 20", e.name
-#   end
-#
-#   def test_enrollment_can_return_kindergarten_participation_by_year
-#     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
-#
-#     assert_equal e.kindergarten_participation_by_year, {2010 => 0.391, 2011 => 0.353, 2012 => 0.267}
-#   end
-#
-#   def test_enrollment_can_return_kindergarten_participation_in_specific_year
-#
-#     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
-#
-#     assert_equal 0.391, e.kindergarten_participation_in_year(2010)
-#     assert_equal nil, e.kindergarten_participation_in_year(2004)
-#   end
-#
-# end
+require_relative 'test_helper'
+require_relative '../lib/enrollment.rb'
+
+class EnrollmentTest < Minitest::Test
+
+  def test_enrollment_holds_enrollment_data_for_single_district
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal "ACADEMY 20", e.name
+  end
+
+  def test_enrollment_can_return_kindergarten_participation_by_year
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal e.kindergarten_participation_by_year, {2010 => 0.391, 2011 => 0.353, 2012 => 0.267}
+  end
+
+  def test_enrollment_can_return_kindergarten_participation_in_specific_year
+
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal 0.391, e.kindergarten_participation_in_year(2010)
+    assert_equal nil, e.kindergarten_participation_in_year(2004)
+  end
+
+  def test_it_can_return_kindergarten_participation_for_a_district_in_a_specific_year
+  er = EnrollmentRepository.new
+  er.load_data({
+                 :enrollment => {
+                   :kindergarten => "./data/Kindergartners in full-day program.csv"
+                 }
+               })
+
+  name = "GUNNISON WATERSHED RE1J"
+  enrollment = er.find_by_name(name)
+  assert_equal name, enrollment.name
+  assert enrollment.is_a?(Enrollment)
+  # binding.pry
+  assert_in_delta 0.144, enrollment.kindergarten_participation_in_year(2004), 0.005
+  end
+
+end
